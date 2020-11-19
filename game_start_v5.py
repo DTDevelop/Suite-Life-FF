75aa2dd5cb0046fb8cf41faf73a5456573419e62
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2020-11-18 17:34:46 -0800

    Add files via upload

--- game_start_v5.py
+++ game_start_v5.py
@@ -5,6 +5,7 @@ import pygame
 import sprite_class as spr
 import create_character as char
 
+
 pygame.init()
 
 """
@@ -26,25 +27,34 @@ char_objects = char.create_characters(char.scale_char_imgs(
                char.load_assets("relative_directory.txt")),DISPLAY_WIDTH,DISPLAY_HEIGHT)
 CHR_WIDTH = char_objects[0].get_img().get_width()
 CHR_HEIGHT = char_objects[0].get_img().get_height()
+
+char_objects[1].set_x(char_objects[1].get_x()*2)
 #blit function under construction - refer create_character.py
 # char_info = char.blit_character(char_objects)
 
 """
 KEY INPUT DICT
 """
+# def walk_up():
+#     pass
+# def walk_down():
+#     pass
+# def walk_left():
+#     pass
+# def walk_right():
+#     pass
 #if certain key press, calls on different char objects within list
-key_press_one = {
-pygame.K_UP: walk_up,
-pygame.K_DOWN: walk_down,
-pygame.K_LEFT: walk_left,
-pygame.K_RIGHT: walk_right
+player_one_mapping = {
+'up': char_objects[0].walk_up,
+'down': char_objects[0].walk_down,
+'left': char_objects[0].walk_left,
+'right': char_objects[0].walk_right
 }
-
-key_press_two = {
-pygame.K_w: walk_up,
-pygame.K_s: walk_down,
-pygame.K_a: walk_left,
-pygame.K_d: walk_right
+player_two_mapping = {
+'up': char_objects[1].walk_up,
+'down': char_objects[1].walk_down,
+'left': char_objects[1].walk_left,
+'right': char_objects[1].walk_right
 }
 
 """
@@ -55,20 +65,40 @@ while not end:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
-
-    gameDisplay.fill((102,204,0))
+    # pressed = pygame.key.get_pressed()[pygame.K_w]
+    # if pressed == pygame.K_w:
+    #     char_objects[1].walk_up()
     for char in char_objects:#TEMP BLIT, EXPLICIT
         gameDisplay.blit(char.get_img(),char.get_pos(),(0,0,CHR_WIDTH/8,CHR_HEIGHT/8))
+    #     #has black background because of convert
+    if event.type == pygame.KEYDOWN:
+        pressed = pygame.key.get_pressed() #checked only if keydown event
 
-    pressed = pygame.key.get_pressed()
-    if key_press in key_press_one:
-        char_objects[0].key_press_one[key_press]()
-    if key_press in key_press_two:
-        char_objects[1].key_press_two[key_press]()
+        if pressed[pygame.K_UP]:
+            player_one_mapping['up']()
+        if pressed[pygame.K_DOWN]:
+            player_one_mapping['down']()
+        if pressed[pygame.K_LEFT]:
+            player_one_mapping['left']()
+        if pressed[pygame.K_RIGHT]:
+            player_one_mapping['right']()
+
+        if pressed[pygame.K_w]:
+            player_two_mapping['up']()
+        if pressed[pygame.K_s]:
+            player_two_mapping['down']()
+        if pressed[pygame.K_a]:
+            player_two_mapping['left']()
+        if pressed[pygame.K_d]:
+            player_two_mapping['right']()
+
+
+
+        #do for all keys
 
     pygame.display.update()
     clock.tick(60)
-
+print(char_objects[1].get_pos())
 pygame.quit()
 quit()
 
